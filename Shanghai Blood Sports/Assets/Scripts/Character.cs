1cc1ace841a17f1c6893f9c452e8f7b03f569ac1
Dominic Trottier <domtrottier@outlook.com>
2019-01-25 20:58:35 -0700

    Added Character prefab

---
+++ Shanghai Blood Sports/Assets/Scripts/Character.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private TextMesh healthIndicator;
+    private double health = 100;
+    private double guard = 100;
+
+    public double Health {
+        get => health;
+        set {
+            health = value;
+            healthIndicator.text = $"Health: {health}\nGuard: {guard}";
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        healthIndicator = transform.GetChild(1).GetComponent<TextMesh>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
