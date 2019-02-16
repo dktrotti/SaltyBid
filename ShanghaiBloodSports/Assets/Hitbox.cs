f3c106977a8386281b7dd4c880d00e78f8879130
Dominic Trottier <domtrottier@outlook.com>
2019-02-16 16:19:33 -0700

    Added hitbox component

---
+++ ShanghaiBloodSports/Assets/Hitbox.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Hitbox : MonoBehaviour
+{
+    public bool Active { get; set; } = false;
+
+    private Character owner {
+        get {
+            return GetComponentInParent<Character>();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        Hitbox other = col.gameObject.GetComponent<Hitbox>();
+        if (other != null && Active && !other.Active)
+        {
+            // TODO: Find a way to deactive the entire move?
+            // This allows the same move to hit multiple times with different hitboxes
+            Active = false;
+
+            // TODO: Emit hit event with owner's current move
+        }
+    }
+}
