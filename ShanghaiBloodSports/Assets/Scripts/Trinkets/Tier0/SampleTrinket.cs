ee0ca8b53d43a703343b8be4d3fa7c6b265de6d3
Dominic Trottier <dktrotti@ualberta.ca>
2019-02-17 14:51:30 -0700

    Added sample trinket as scriptable object

---
+++ ShanghaiBloodSports/Assets/Scripts/Trinkets/Tier0/SampleTrinket.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Assets.Scripts.Events;
+
+public class SampleTrinket : TrinketBase
+{
+    private Sprite sprite = Resources.Load<Sprite>("Assets/Art/Sprites/punch");
+    private EventHandler handler = new Handler();
+
+    public override string Name => "Sample Trinket";
+    public override string Description => "This is a sample trinket";
+    public override Sprite Sprite => sprite;
+    public override EventHandler EventHandler => handler;
+
+    private class Handler : EventHandler
+    {
+        public override void onEvent(HitEvent e)
+        {
+            Debug.Log("Hit event occurred");
+        }
+    }
+}
